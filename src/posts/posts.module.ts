729cb4dba563d5b11171e57584928aa1a622ec71
pierrialexandervidmar <pierri@hotmail.com.br>
2024-02-18 22:55:48 -0300

    Melhorias nos relacionamentos

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { PostRepository } from './repositories/posts.repository';
 
 @Module({
   controllers: [PostsController],
-  providers: [PostsService, PrismaService],
+  providers: [PostsService, PrismaService, PostRepository],
 })
 export class PostsModule {}
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,26 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { PostRepository } from './repositories/posts.repository';
+import { NotFoundError } from 'src/common/errors/types/NotFoundError';
 
 @Injectable()
 export class PostsService {
+  constructor(private readonly repository: PostRepository) {}
+
   create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+    return this.repository.create(createPostDto);
   }
 
   findAll() {
-    return `This action returns all posts`;
+    return this.repository.findAll();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
+  async findOne(id: number) {
+    const post = await this.repository.findOne(id);
+
+    if (!post) {
+      throw new NotFoundError('Post não encontrado!');
+    }
+
+    return post;
   }
 
   update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
+    return this.repository.update(id, updatePostDto);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} post`;
+    return this.repository.remove(id);
   }
 }
